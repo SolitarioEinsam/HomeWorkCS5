3220edb30717dab26d3d9b938834e39e4ff89a8d
Alex <leha.pavlov.03.03@mail.ru>
2023-10-09 20:58:55 +0700

    all done

---
+++ 1/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+using System.Diagnostics;
+using System.Runtime.Serialization.Formatters;
+
+void arrFull(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"Введите значение для {i} элемента массива: ");
+        arr[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    System.Console.Write("Введенный массив:");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        System.Console.Write($"{arr[i]} ");
+    }
+    System.Console.WriteLine();
+}
+
+int surchNegativeCount(int[] arr)
+{
+    int index = 0;
+    for (int i = 0; i < arr.Length; i ++)
+    {
+        if (arr[i] < 0)
+        index ++;
+    }
+    return index;
+}
+
+
+System.Console.Write("Введите число элемнотов в массиве: ");
+int arrLength = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[arrLength];
+arrFull(array);
+System.Console.WriteLine($"Элементов с отрицательным значением: {surchNegativeCount(array)}");
---
+++ 2/Program.cs
@@ -0,0 +1,15 @@
+﻿
+double k1, b1, k2, b2;
+System.Console.Write("Введите значение для параметра k1: ");
+k1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите значение для параметра b1: ");
+b1 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите значение для параметра k2: ");
+k2 = Convert.ToInt32(Console.ReadLine());
+System.Console.Write("Введите значение для параметра b2: ");
+b2 = Convert.ToInt32(Console.ReadLine());
+
+double x = (b2 - b1) / (k1 - k2);
+double y = k1 * x + b1;
+
+System.Console.WriteLine($"Координаты точки пересечения функций: ({x}, {y})");
